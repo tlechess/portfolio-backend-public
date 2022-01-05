16a485148439c145f47f1600dc44237086fc1ced
Tyler <tjltestes@gmail.com>
2022-01-05 18:02:03 -0500

    Added a email api

---
+++ src/main/java/com/portfolio/controllers/EmailController.java
@@ -0,0 +1,23 @@
+package com.portfolio.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portfolio.models.Email;
+import com.portfolio.services.EmailService;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+	
+	@PostMapping("/contact-form")
+	public ResponseEntity<Email> sendEmailMessage(@RequestBody Email email) {
+		
+		return emailService.sendMessage(email);
+	}
+}
