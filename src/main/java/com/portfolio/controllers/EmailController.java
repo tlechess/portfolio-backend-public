4aa4b268af42bdfa71c37b95771795d914e06834
Tyler <tjltestes@gmail.com>
2022-01-05 19:26:22 -0500

    added cross origin

--- src/main/java/com/portfolio/controllers/EmailController.java
+++ src/main/java/com/portfolio/controllers/EmailController.java
@@ -2,6 +2,7 @@ package com.portfolio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class EmailController {
 	@Autowired
 	private EmailService emailService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/contact-form")
 	public ResponseEntity<Email> sendEmailMessage(@RequestBody Email email) {
 		
